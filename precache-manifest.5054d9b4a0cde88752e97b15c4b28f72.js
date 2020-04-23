20ca17d0a232554726fd2d293791d3d4743b97cb
clinedim <marc.clinedinst@gmail.com>
2020-04-22 21:03:55 -0400

    Updates

---
+++ precache-manifest.5054d9b4a0cde88752e97b15c4b28f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e24f7ca948b56465f3402e36ce5d2db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f4695a700d6257bab6d",
+    "url": "/monsters-rolodex/static/css/main.6b70d275.chunk.css"
+  },
+  {
+    "revision": "77ecff62be75e32fda66",
+    "url": "/monsters-rolodex/static/js/2.b9bcc37e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b9bcc37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4695a700d6257bab6d",
+    "url": "/monsters-rolodex/static/js/main.6ae17c74.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
